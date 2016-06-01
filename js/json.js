6c9ae6a5c3adc6f188ab5501a8fb391afcdcdfe3
Perlou <a87387875@163.com>
2016-06-01 20:49:48 +0800

    init

---
+++ js/json.js
@@ -0,0 +1,114 @@
+/**
+ * @author Perlou
+ * @json.js
+ */
+
+var Json = {
+    project:[
+        {
+            name:'粤通卡门户网站',
+            date:'公司项目',
+            detail:'粤通卡',
+            image:['images/photo/1/1.png','images/photo/1/2.png','images/photo/1/3.png','images/photo/1/4.png'],
+            link:'http://www.96533.com/'
+        },
+        {
+            name:'华软内刊app',
+            date:'公司项目',
+            detail:'ionic angularjs',
+            image:['images/photo/2/1.png','images/photo/2/2.png','images/photo/2/3.png','images/photo/2/4.png'],
+            link:''
+        },
+        {
+            name:'乐享黔程微信公众号',
+            date:'公司项目',
+            detail:'乐享黔程微信公众号',
+            image:['images/photo/3/1.png','images/photo/3/2.png','images/photo/3/3.png','images/photo/3/4.png'],
+            link:''
+        },
+        {
+            name:'韶赣高速收费培训系统',
+            date:'公司项目',
+            detail:'韶赣高速收费培训系统',
+            image:['images/photo/4/1.png','images/photo/4/2.png','images/photo/4/3.png','images/photo/4/4.png'],
+            link:''
+        },
+        {
+            name:'Voxel',
+            date:'个人项目',
+            detail:'Voxel',
+            image:['images/photo/5/1.png','images/photo/5/2.png','images/photo/5/3.png','images/photo/5/4.png'],
+            link:'perlou.github.io/Voxel'
+        },
+        {
+            name:'移动端打怪小游戏',
+            date:'个人项目',
+            detail:'h5',
+            image:['images/photo/6/1.png','images/photo/6/2.png','images/photo/6/3.png','images/photo/6/4.png'],
+            link:'perlou.github.io/MonsterGame'
+        },
+        {
+            name:'Diagnosite',
+            date:'个人项目',
+            detail:'Dia',
+            image:['images/photo/7/1.png','images/photo/7/2.png','images/photo/7/3.png','images/photo/7/4.png'],
+            link:'perlou.github.io/Diagnosite'
+        },
+        {
+            name:'贵州电子支付网',
+            date:'个人项目',
+            detail:'Dia',
+            image:['images/photo/8/1.png','images/photo/8/2.png','images/photo/8/3.png','images/photo/8/4.png'],
+            link:'perlou.github.io/Diagnosite'
+        },
+        {
+            name:'Diagnosite',
+            date:'个人项目',
+            detail:'Dia',
+            image:['images/photo/9/1.png','images/photo/9/2.png','images/photo/9/3.png','images/photo/9/4.png'],
+            link:'perlou.github.io/Diagnosite'
+        },
+        {
+            name:'Diagnosite',
+            date:'个人项目',
+            detail:'Dia',
+            image:['images/photo/10/1.png','images/photo/10/2.png','images/photo/10/3.png','images/photo/10/4.png'],
+            link:'perlou.github.io/Diagnosite'
+        },
+        {
+            name:'Diagnosite',
+            date:'个人项目',
+            detail:'Dia',
+            image:['images/photo/11/1.png','images/photo/11/2.png','images/photo/11/3.png','images/photo/11/4.png'],
+            link:'perlou.github.io/Diagnosite'
+        }
+    ],
+    work: [
+        {
+            company:'js23',
+            job:'234242',
+            date:'343434',
+            detail:'343434',
+            logo:'images/icons/header1.png'
+        }
+    ],
+    myInfo:{
+        name:'钟嘉文',
+        slogan:'前端开发工程师',
+        edu:'本科(吉林大学珠海学院)',
+        time:'一年半',
+        sex:'男',
+        city:'广州',
+        phone:'13726245011',
+        mail:'a87387875@163.com',
+        work:'前端开发工程师， 广州',
+        work1:'',
+        nature:'',
+        github: '<a target="blank" href="https://github.com/Perlou">https://github.com/Perlou</a>',
+        hopeCity:'广州',
+        money:'233333',
+        head:'images/photo/head.jpg',
+        ps:''
+    }
+
+};

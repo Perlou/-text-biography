31051fe24ea23aaebcdd3519f61d7d7f57d94704
Perlou <a87387875@163.com>
2016-06-05 12:57:57 +0800

    build

--- dist/js/index.js
+++ dist/js/index.js
@@ -13,6 +13,7 @@ window.onload = function(){
     var myInfoChild = myInfo.children;
     var aDiv = myInfoChild[0].getElementsByTagName('div');
     var img = document.createElement('img');
+    Skill();
     img.src = Json.myInfo.head;
     aDiv[0].appendChild(img);
 
@@ -92,6 +93,69 @@ window.onload = function(){
 
     }
 
-
-
 };
+
+function Skill(){
+    console.log('skill');
+    var oSkill = document.getElementById('skill'),
+        oSkillUl = document.getElementById('skillUl'),
+        oSkillTable = document.getElementById('skillTable'),
+        aSkillLi = oSkillUl.getElementsByTagName('li'),
+        aSkillA = oSkillTable.getElementsByTagName('a'),
+        oldIndex = 0;
+
+    Skilltable();
+    ChartsMove();
+
+    function Skilltable(){
+
+        for(var i=0; i<aSkillLi.length; i++){
+
+            var oA = document.createElement('a');
+            oA.href = 'javascript:;';
+            oSkillTable.appendChild(oA);
+            aSkillLi[i].index = i;
+
+        }
+
+        aSkillLi[0].className = 'active';
+        aSkillA[0].className = 'active';
+
+    }
+
+    function ChartsMove(){
+
+        for(var i=0; i<aSkillA.length; i++){
+
+            aSkillA[i].index = i;
+            aSkillA[i].onclick = function(){
+                Move(this.index);
+            }
+
+        }
+
+    }
+
+    function Move(index){
+
+        for(var i=0; i<aSkillA.length; i++){
+
+            aSkillA[i].className = '';
+
+        }
+
+        aSkillA[index].className = 'active';
+
+        if(index > oldIndex){
+            aSkillLi[index].className = 'rightShow';
+            aSkillLi[oldIndex].className = 'leftHide';
+        }else if(index < oldIndex){
+            aSkillLi[index].className = 'leftShow';
+            aSkillLi[oldIndex].className = 'rightHide';
+        }
+
+        oldIndex = index;
+
+    }
+
+}
